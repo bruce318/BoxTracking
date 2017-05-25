11eb52f04fbd0c2875857a1b99911f4a7c5654da
BoyangLi <boyangli318@gmail.com>
2017-05-24 23:16:08 -0700

    inputPath

--- WordTracking2/main.cpp
+++ WordTracking2/main.cpp
@@ -13,9 +13,13 @@ int main(int argc, const char * argv[]) {
     //choose a input type video or images
     bool videoInput = false;//true is vedio input, false is images input
     //path of the source
-//    std::string inputPath= "/Users/boyang/workspace/BoxTracking/srcVideo/IMG_7087.MOV";//a video src
-    std::string inputPath= "/Users/boyang/workspace/BoxTracking/src";//a image src
-    
+    std::string inputPath;
+    if (videoInput) {
+        std::string inputPath= "/Users/boyang/workspace/BoxTracking/srcVideo/IMG_7087.MOV";//a video src
+    } else {
+        std::string inputPath= "/Users/boyang/workspace/BoxTracking/src";//a image src
+    }
+
     //start
     return Tracking::doTracking(videoInput, inputPath);
     
