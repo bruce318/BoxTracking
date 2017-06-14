900d10b74eff7ee9e14f7a3477406b82de53d2ec
BoyangLi <boyangli318@gmail.com>
2017-06-14 02:26:54 -0700

    clear file

--- WordTracking2/main.cpp
+++ WordTracking2/main.cpp
@@ -15,13 +15,22 @@ int main(int argc, const char * argv[]) {
     bool videoInput = true;//true is vedio input, false is images input
     //path of the source
     std::string inputPath;
-    if (videoInput) {
-        inputPath= "/Users/boyang/workspace/BoxTracking/videoInput/1.mp4";//a video src
+    std::string filePath;
+    if (argc != 2) {
+        if (videoInput) {
+            inputPath= "/Users/boyang/workspace/BoxTracking/videoInput/1.mp4";//a video src
+        } else {
+            inputPath= "/Users/boyang/workspace/BoxTracking/imageInput/1/";//a image src
+        }
+        filePath = "/Users/boyang/workspace/BoxTracking/boxCoordinates/1.txt";
     } else {
-        inputPath= "/Users/boyang/workspace/BoxTracking/imageInput/";//a image src
+        inputPath = argv[0];
+        filePath = argv[1];
     }
+    
+    
     //load rectangle box's coordinates
-    ReadRectFromFile::loadRects("/Users/boyang/workspace/BoxTracking/boxCoordinates/1.txt");
+    ReadRectFromFile::loadRects(filePath);
     
     //start
     return Tracking::doTracking(videoInput, inputPath);
