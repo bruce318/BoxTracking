453e7f9ad356c3881c31138d509e1f5c8217e6fa
BoyangLi <boyangli318@gmail.com>
2017-06-22 00:13:57 -0700

    add forJNI

---
+++ WordTracking2/forJNI.hpp
@@ -0,0 +1,14 @@
+//
+//  forJNI.hpp
+//  WordTracking2
+//
+//  Created by boyang on 6/22/17.
+//  Copyright © 2017 boyang. All rights reserved.
+//
+
+#ifndef forJNI_hpp
+#define forJNI_hpp
+
+#include <stdio.h>
+
+#endif /* forJNI_hpp */
